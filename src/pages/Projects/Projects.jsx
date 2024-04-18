b3c097988c67aeeacfd5f49373efd49cb685a355
Bishoy <bishoy.demyan@gmail.com>
2024-04-19 01:58:59 +0200

    Projects design

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -4,6 +4,8 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import img from "../../assets/images/slider-image-2.jpeg"
 import imgProject from "../../assets/images/head_project.png"
+import projectImg from "../../assets/images/image6.png"
+import { useNavigate } from "react-router-dom";
 
 
 export default function Projects() {
@@ -14,6 +16,7 @@ export default function Projects() {
     slidesToShow: 1,
     slidesToScroll: 1,
   };
+  let nav = useNavigate();
   return (
     <>
     <div className="d-flex justify-content-center w-100 align-items-center bg-danger">
@@ -32,7 +35,7 @@ export default function Projects() {
       <div className='container mt-5 pt-5'>
      <div className='mt-5 pt-5'>
      <div className='d-flex justify-content-between'>
-      <h2>Learn more about our projects</h2>
+      <h2>Learn more about our projects <br/>Lorem ipsum dolor sit amet.</h2>
      </div>
       </div>
   
@@ -49,46 +52,33 @@ export default function Projects() {
 <div className="card-body">
   <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 </div>
+</div>
+<div className="card col-md-3 mx-auto mt-4 w-50 text-center bg-danger d-flex justify-content-center ">
+<img src={img} className="w-100 " alt="..." />
+<div className="card-body">
+  <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+</div>
 </div>    
   </Slider>
   
 </div>
-<div className='customDiv mb-5'>
 
-<div className="container">
-<div className="row justify-content-center  align-content-center align-items-center">
-<div className="d-flex align-self-center w-25 align-items-center mt-5">
-<div className='px-3  align-content-end'>
-<p className=' mt-4 mb-0 fs-2 text-white'>350</p>
-<p className=' mb-4 mt-0 pt-0 fs-5 text-white'>Employees</p>
+
+
+<div className="container-fluid">
+<div className="row">
+<div className="col-md-6 ProjectsCustom pt-5">
+<h2>Lorem ipsum dolor sit amet.</h2>
+<p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Aut, autem.</p>
+<button className='customBtn btn p-3  text-center' onClick={()=>nav("/contactus")}>Contact Us <i class="fa-solid fa-phone-volume"></i></button>
+
 </div>
-<div className="customsqr d-flex justify-content-center align-items-center"><i class="fa-solid fa-users fs-4"></i></div>
-</div>
-<div className="d-flex align-self-center w-25 align-items-center mt-5">
-<div className='px-3  align-content-end'>
-<p className=' mt-4 mb-0 fs-2 text-white'>350</p>
-<p className=' mb-4 mt-0 pt-0 fs-5 text-white'>Employees</p>
-</div>
-<div className="customsqr d-flex justify-content-center align-items-center"><i class="fa-solid fa-users fs-4"></i></div>
-</div>
-<div className="d-flex align-self-center w-25 align-items-center mt-5">
-<div className='px-3  align-content-end'>
-<p className=' mt-4 mb-0 fs-2 text-white'>350</p>
-<p className=' mb-4 mt-0 pt-0 fs-5 text-white'>Employees</p>
-</div>
-<div className="customsqr d-flex justify-content-center align-items-center"><i class="fa-solid fa-users fs-4"></i></div>
+<div className="col-md-6  ProjectsCustom">
+<img src={projectImg} className="w-75  h-100  p-5" alt="" />
 </div>
 </div>
 </div>
 
-</div>
-<div className='text-center'>
-<h2 className='text-center m-2 mb-4 fs-2 fw-bolder'>OUR TOP CUSTOMERS</h2>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Totam dolorum pariatur quidem! Assumenda, voluptas sed.</p>
-</div>
-<div>
-<img src={img} className='w-100 mb-5 mt-5' height= "200px" alt="" />
-</div>
 
     </>
   );
