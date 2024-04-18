0d36e9360d9c4f1ced78b01536125f29383c2fe7
Bishoy <bishoy.demyan@gmail.com>
2024-04-18 22:20:29 +0200

    Routing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 import './App.css';
 import Layout from './Components/Layout/Layout';
-import Service from './Components/Service/Service';
-import Workshoplayout from './WorkshopLayout/Workshoplayout';
 import Dynamicbalance from './Components/DynamicBalancing/Dynamicbalance';
 import Welding from './Components/Welding/Welding';
 import Metal from './Components/Metal/Metal';
-import Home from './Components/Home/Home';
+import Workshop from './Components/Workshop/Workshop';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Service from './pages/Service/Service';
 import Contactus from './pages/Contactus/Contactus';
 import Forming from './Components/Forming/Forming';
-import About from './pages/About/About';
 import Pressing from './Components/Pressing/Pressing';
 import Hydraulic from './Components/Hydraulic/Hydraulic';
 import Wire from './Components/Wire/Wire';
+import Projects from './pages/Projects/Projects';
 
 function App() {
  let routes =  createBrowserRouter([
   {path:"", element:<Layout/> , children:[{index:"true",element:<Home/>},
   {path:"aboutus" , element:<About/>},
   {path:"service" , element:<Service/>},
+  {path:"workshop" , element:<Workshop/>},
+  {path:"projects", element:<Projects/>},
   {path:"home" , element:<Home/> , children:[
     {path:"forming", element:<Forming/>},
     {path:"dynamic", element:<Dynamicbalance/>},
@@ -38,12 +41,13 @@ function App() {
  ])
   return (
     <>
-      <RouterProvider router={routes}>
-        <Home />
-        <About />
-      </RouterProvider>
+    <RouterProvider router={routes}>
+   <Home/>
+   <About/>
+    </RouterProvider>
+    
     </>
-  );
+);
 }
 
 export default App;
