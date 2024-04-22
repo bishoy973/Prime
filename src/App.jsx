2eab46cf9496b95508ce2906e7740ce4a50e3596
bishoymelek <69328362+bishoymelek@users.noreply.github.com>
2024-04-22 02:16:03 +0200

    add eslint, prettier, ts, vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,23 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { useTranslation } from "react-i18next";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "./App.css";
-import Layout from "./Components/Layout/Layout";
-import Dynamicbalance from "./Components/DynamicBalancing/Dynamicbalance";
-import Welding from "./Components/Welding/Welding";
-import Metal from "./Components/MetalWork/Metalwork";
-import Workshop from "./Components/Workshop/Workshop";
-import Home from "./pages/Home/Home";
+
 import About from "./pages/About/About";
-import Service from "./pages/Service/Service";
 import Contactus from "./pages/Contactus/Contactus";
-import Forming from "./Components/Forming/Forming";
-import Pressing from "./Components/Pressing/Pressing";
-import Hydraulic from "./Components/Hydraulic/Hydraulic";
-import Wire from "./Components/Wire/Wire";
+import Home from "./pages/Home/Home";
 import Projects from "./pages/Projects/Projects";
+import Service from "./pages/Service/Service";
+
+import Dynamicbalance from "./Components/DynamicBalancing/Dynamicbalance";
+import Forming from "./Components/Forming/Forming";
+import Hydraulic from "./Components/Hydraulic/Hydraulic";
+import Layout from "./Components/Layout/Layout";
+import Metal from "./Components/MetalWork/Metalwork";
+import Pressing from "./Components/Pressing/Pressing";
+import Welding from "./Components/Welding/Welding";
+import Wire from "./Components/Wire/Wire";
+import Workshop from "./Components/Workshop/Workshop";
 
 function App() {
   const { i18n } = useTranslation();
@@ -26,8 +28,8 @@ function App() {
       path: "",
       element: <Layout />,
       children: [
-        { index: "true", element: <Home /> },
-        { path: "aboutus", element: <About /> },
+        { index: true, element: <Home /> },
+        { path: "about-us", element: <About /> },
         { path: "service", element: <Service /> },
         { path: "workshop", element: <Workshop /> },
         { path: "projects", element: <Projects /> },
@@ -44,19 +46,12 @@ function App() {
             { path: "wire", element: <Wire /> },
           ],
         },
-        { path: "contactus", element: <Contactus /> },
+        { path: "contact-us", element: <Contactus /> },
       ],
     },
   ]);
 
-  return (
-    <>
-      <RouterProvider router={routes}>
-        <Home />
-        <About />
-      </RouterProvider>
-    </>
-  );
+  return <RouterProvider router={routes} />;
 }
 
 export default App;
